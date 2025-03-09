aee107cbdb009a750491fead41924f4bd7fbe0f8
HEXA <aminerico885@gmail.com>
2025-03-09 18:00:54 +0100

    Create proxies.py

---
+++ proxies.py
@@ -0,0 +1,9 @@
+import requests
+
+def get_tor_session():
+    session = requests.Session()
+    session.proxies = {
+        'http': 'socks5h://127.0.0.1:9050',
+        'https': 'socks5h://127.0.0.1:9050'
+    }
+    return session
